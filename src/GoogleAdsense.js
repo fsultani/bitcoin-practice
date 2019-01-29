9a42b03684395894010cfd3f3fa70ac5aa11fefc
Farid Sultani <fsultani@farids-mbp.home>
2019-01-28 23:48:23 -0800

    Add Google Adsense

---
+++ src/GoogleAdsense.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class Ad extends Component {
+  componentDidMount() {
+    window.adsbygoogle = window.adsbygoogle || [].push({});
+  }
+
+  render() {
+    return (
+      <div>
+        <ins
+          className="adsbygoogle"
+          style={{ display: 'block' }}
+          data-ad-client="ca-pub-7964699062377847"
+          data-ad-slot="4833844307"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        />
+      </div>
+    );
+  }
+}
